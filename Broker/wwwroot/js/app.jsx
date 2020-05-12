2fb208b15bcf5caaf4e9ece6bba4efe4b4233e49
Tenq <redpowerall@gmail.com>
2020-05-12 13:35:28 +0300

    test again

--- Broker/wwwroot/js/app.jsx
+++ Broker/wwwroot/js/app.jsx
@@ -1,37 +1,23 @@
 ﻿import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Navbar } from './Components/Navbar'
-import { NavLink } from 'react-router-dom'
-
-export const Navbar = () => (
-    <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
-        <div className="navbar-brand">Broker</div>
-
-        <ul className="navbar-nav">
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/" exact>Home</NavLink>
-            </li>
-            <li className="nav-item">
-                <NavLink className="nav-link" to="/"> Stocks</NavLink>
-            </li>
-        </ul>
-    </nav>
-)
+import { Navbar } from './components/Navbar'
 
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Navbar />
-            <div className="container pt-4">
-                <Alert />
-                <Switch>
-                    <Route path={'/'} exact component={Home} />
 
-                </Switch>
-            </div>
-        </BrowserRouter>
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Navbar />
+                <div className="container pt-4">
+                    <Alert />
+                    <Switch>
+                        <Route path={'/'} exact component={Home} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
+    }
 }
 ReactDOM.render(
     <App />,
